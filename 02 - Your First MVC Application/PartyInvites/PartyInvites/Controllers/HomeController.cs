e423132581d93961a7d3a8e1d7f0760e09eebef2
Irina Hristova <hristova119@gmail.com>
2018-04-07 15:17:22 +0300

    Added chapter 2 training

--- 02 - Your First MVC Application/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ 02 - Your First MVC Application/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -3,33 +3,42 @@ using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 using System.Linq;
 
-namespace PartyInvites.Controllers {
+namespace PartyInvites.Controllers
+{
 
-    public class HomeController : Controller {
+    public class HomeController : Controller
+    {
 
-        public ViewResult Index() {
+        public ViewResult Index()
+        {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
 
         [HttpGet]
-        public ViewResult RsvpForm() {
+        public ViewResult RsvpForm()
+        {
             return View();
         }
 
         [HttpPost]
-        public ViewResult RsvpForm(GuestResponse guestResponse) {
-            if (ModelState.IsValid) {
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
                 Repository.AddResponse(guestResponse);
                 return View("Thanks", guestResponse);
-            } else {
+            }
+            else
+            {
                 // there is a validation error
                 return View();
             }
         }
 
-        public ViewResult ListResponses() {
+        public ViewResult ListResponses()
+        {
             return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
